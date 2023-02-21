cbd9d659643c223a0715b3fdf2ee47a471600780
Jon Pugh <jon@thinkdrop.net>
2023-02-21 12:22:22 -0500

    Attempting to create ansi formatter.

---
+++ web/modules/devshop_task/src/Plugin/Field/FieldFormatter/AnsiOutputFormatter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\devshop_task\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
+
+/**
+ * Plugin implementation of the 'ANSI Output' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "devshop_task_ansi_output",
+ *   label = @Translation("ANSI Output"),
+ *   field_types = {
+ *     "string",
+ *     "string_long",
+ *   }
+ * )
+ */
+class AnsiOutputFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'foo' => 'bar',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+
+    $elements['foo'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Foo'),
+      '#default_value' => $this->getSetting('foo'),
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary[] = $this->t('Foo: @foo', ['@foo' => $this->getSetting('foo')]);
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+//    $converter = new AnsiToHtmlConverter();
+    foreach ($items as $delta => $item) {
+      $element[$delta] = [
+        '#markup' => $item->value,
+      ];
+    }
+
+    return $element;
+  }
+
+}
