e57d45005fcbfd64e13b6ce71196ca05f9e477a2
Jon Pugh <jon@thinkdrop.net>
2023-06-22 10:01:28 -0400

    Create global config overrides setting and use it in SiteEntity::preSave.

--- modules/site_manager/src/Form/SettingsForm.php
+++ modules/site_manager/src/Form/SettingsForm.php
@@ -4,6 +4,8 @@ namespace Drupal\site_manager\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Configure Site Manager settings for this site.
@@ -28,10 +30,11 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['example'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('site_manager.settings')->get('example'),
+    $form['global_config_overrides'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Global Site Config Overrides'),
+      '#description' => $this->t('Enter YAML to load into all Site entities.'),
+      '#default_value' => $this->config('site_manager.settings')->get('global_config_overrides'),
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -40,8 +43,15 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
+    try {
+      $settings = Yaml::parse($form_state->getValue('global_config_overrides'));
+      if (is_string($settings) || !is_string(key($settings))) {
+        $form_state->setErrorByName('global_config_overrides', $this->t('Global Site Config Overrides must be a yaml mapping. Use the format "name: value".'));
+      }
+    } catch (ParseException $e) {
+      $form_state->setErrorByName('global_config_overrides', $this->t('Invalid Yaml: :message', [
+        ':message' => $e->getMessage(),
+      ]));
     }
     parent::validateForm($form, $form_state);
   }
@@ -51,7 +61,7 @@ class SettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('site_manager.settings')
-      ->set('example', $form_state->getValue('example'))
+      ->set('global_config_overrides', $form_state->getValue('global_config_overrides'))
       ->save();
     parent::submitForm($form, $form_state);
   }
