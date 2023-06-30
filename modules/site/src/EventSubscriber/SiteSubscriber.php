33ba2ba51dd903bb3f035844450616bacae6943a
Jon Pugh <jon@thinkdrop.net>
2023-06-30 08:34:14 -0400

    Finish storage and display of config changes.

--- modules/site/src/EventSubscriber/SiteSubscriber.php
+++ modules/site/src/EventSubscriber/SiteSubscriber.php
@@ -31,12 +31,23 @@ class SiteSubscriber implements EventSubscriberInterface {
    *   Response event.
    */
   public function onConfigSave(ConfigCrudEvent $event) {
+
     $site = SiteDefinition::load('self');
     if ($site->get('settings')['save_on_config']) {
-      $site->saveEntity(t('Config ":config" saved by ":user" at :url.', [
+      $data = $site->get('data');
+      $data['config_changes'] = [
+        'original' => $event->getConfig()->getOriginal(),
+        'new' => $event->getConfig()->get(),
+        'user' => \Drupal::currentUser()->getDisplayName(),
+        'ip' => \Drupal::request()->getClientIp(),
+        'url' => \Drupal::request()->getUri(),
+      ];
+      $site->set('data', $data);
+      $site->saveEntity(t('Config ":config" updated at :url by ":user" (:ip)', [
         ':config' => $event->getConfig()->getName(),
         ':user' => \Drupal::currentUser()->getDisplayName(),
         ':url' => \Drupal::request()->getUri(),
+        ':ip' => \Drupal::request()->getClientIp(),
       ]));
     }
   }
