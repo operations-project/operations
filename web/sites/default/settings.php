864cdb33d2f9380904e1d496c41ee58c9f1cc1c1
Jon Pugh <jon@thinkdrop.net>
2023-07-08 07:32:47 -0400

    Fix settings for lando.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -11,13 +11,7 @@ $projects = [
   'mars.lndo.site' => 'mars',
 ];
 
-if (isset($projects[$_SERVER['HTTP_HOST']])) {
-  $project = $projects[$_SERVER['HTTP_HOST']];
-}
-else {
-  $project = $projects['ox'];
-}
-
+$project = $projects[$_SERVER['HTTP_HOST'] ?? 'vsd'];
 $settings['config_sync_directory'] = "../config/{$project}";
 
 if ((bool) getenv('LANDO')) {
@@ -28,22 +22,18 @@ if ((bool) getenv('LANDO')) {
   $host = strtr($_SERVER['HTTP_HOST'], ['.lndo.site' => '']);
   $database_host = $host . '_db';
 
-  if ($host == 'ox.lndo.site') {
-    $host = 'vsd';
-    $database_host = 'database';
-  }
-  elseif ($host == 'ox') {
+  if ($host == 'ox') {
     $host = 'ox';
     $database_host = 'database';
   }
 
   if ($host){
-    putenv("LANDO_DATABASE_HOST={$database_host}");
+    putenv("LANDO_DATABASE_HOST=$database_host");
     $settings['file_public_path'] = 'sites/' . $host . '/files';
   }
 
   $uuids = [
-    'ox' => 'd3c85e16-e0db-49f1-b7fd-f13e84f3b9dd',
+    'vsd' => 'd3c85e16-e0db-49f1-b7fd-f13e84f3b9dd',
     'mercury' => 'daaa81bd-5f27-4152-a209-511e31368848',
     'mars' => 'site.mars',
     'venus' => 'site.venus',
@@ -51,24 +41,6 @@ if ((bool) getenv('LANDO')) {
   $config['system.site']['uuid'] = $uuids[$host];
 
   $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
-  $settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
-
-# Set ENV vars for site.module
-  putenv('DRUPAL_SITE_HOST_PROVIDER=lando');
-
-# See https://www.drupal.org/docs/getting-started/installing-drupal/trusted-host-settings
-
-# Add all lndo.site urls to trusted_host_patterns.
-  $settings['trusted_host_patterns'] = [
-    # Lando Proxy
-    '\.lndo\.site$',
-    '\.internal$',
-    # Lando Share
-    '\.localtunnel\.me$',
-  ];
-
-  // Do not redirect to www if using httpswww module.
-  $config['httpswww.settings']['prefix'] = 'no';
 
   // To alter what database host is used, set LANDO_DATABASE_HOST.
   $lando_database_host = getenv('LANDO_DATABASE_HOST') ?: 'database';
@@ -80,10 +52,10 @@ if ((bool) getenv('LANDO')) {
   putenv('MYSQL_HOSTNAME=' . $lando_info[$lando_database_host]['internal_connection']['host']);
   putenv('MYSQL_PORT=' . $lando_info[$lando_database_host]['internal_connection']['port']);
 
-  if (file_exists($app_root . '/sites/example.settings.local.php')) {
-    include $app_root . '/sites/example.settings.local.php';
-  }
+
 }
+
+
 
 
 if (empty($databases['default']['default'])) {
@@ -109,40 +81,24 @@ $databases['default']['default']['namespace'] = 'Drupal\\Core\\Database\\Driver\
 
 $settings['rebuild_access'] = FALSE;
 
-//
-///**
-// * Vardot Support Module Settings
-// *
-// * The file below includes acquia/blt settings, Lando config, and other things globally.
-// */
-//require DRUPAL_ROOT . "/modules/contrib/vardot_support/settings/settings.vardot.php";
-
-//$config['site_audit_send.settings']['remote_url'] = 'http://vsd.lndo.site/api/site-audit?api-key=TESTKEY';
-
-// DSD/Site Manager settings.
-
 // The full URL to send site reports to.
 if ($projects[$_SERVER['HTTP_HOST']] != 'ox') {
 
-//
-//// The config items to send along with the report.
-//// use main config name, or value in the format system.site:name
-//  $config['site.site_definition.self']['configs_load'] = [
-//    'core.extension',
-//    'system.cron',
-//    'system.site',
-//  ];
-//  $config['site.site_definition.self']['settings']['save_on_config'] = true;
-//  $config['site.site_definition.self']['settings']['send_on_save'] = true;
-//
-//  # Un hard code for testing.
-//  # $config['site.site_definition.self']['settings']['send_interval'] = 60;
-//  # $config['site.site_definition.self']['settings']['save_interval'] = 0;
-//
-//  if ((bool) getenv('LANDO')) {
-//    $config['site.site_definition.self']['settings']['send_destinations'] = "https://appserver.vsd.internal/api/site/data?api-key=9f6dfc256451638821d1c88f46ea659d";
-//  }
-//  else {
-//    $config['site.site_definition.self']['settings']['send_destinations'] = "https://vsd.demo.support.devshop.cloud/api/site/data?api-key=c2895f91b56dc5b1e952645760a584c8";
-//  }
+  // The config items to send along with the report.
+  // use main config name, or value in the format system.site:name
+  $config['site.site_definition.self']['configs_load'] = [
+    'core.extension',
+    'system.cron',
+    'system.site',
+  ];
+  $config['site.site_definition.self']['settings']['save_on_config'] = true;
+  $config['site.site_definition.self']['settings']['send_on_save'] = true;
+
+  # Un hard code for testing.
+  # $config['site.site_definition.self']['settings']['send_interval'] = 60;
+  # $config['site.site_definition.self']['settings']['save_interval'] = 0;
+
+  if ((bool) getenv('LANDO')) {
+    $config['site.site_definition.self']['settings']['send_destinations'] = "https://ox.lndo.site/api/site/data?api-key=9f6dfc256451638821d1c88f46ea659d";
+  }
 }
